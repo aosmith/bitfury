8531d99068096253fd6018303ef132ce03677188
Alex Smith <aosmith@gmail.com>
2014-03-19 09:05:51 -0700

    Using external config

--- start_miner.sh
+++ start_miner.sh
@@ -4,26 +4,12 @@ sleep 20
 
 _IP=$(hostname -I) || true
 
-# Default env variables
-POOL1_ADDRESS=uk1.ghash.io
-POOL1_PORT=3333
-POOL1_USER=CoinSeed."`echo $_IP | tr '.' 'x'`"
-POOL1_PASS=x
-
-# If this is a restart we need to kill existing processes
-# if [ -z "$MINER_RUNNING" ]; then
-#  echo "Killing miner and proxies..."
-#  screen -wipe miner
-#  screen -wipe stratum1
-#  screen -wipe stratum2
-#  screen -wipe stratum3
-#fi
 
 echo "Updating code and settings..."
 cd /miner && git pull
 
-# echo "Loading environment variables..."
-# . /miner/settings.conf
+echo "Loading environment variables..."
+source /miner/settings.conf
 
 
 echo "Starting proxy..."
